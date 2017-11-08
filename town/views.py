80e3aa4209594fdb712f9153f41c4112eef0e122
Zagrebelsky Oleg <zagrebelsky.o@gmail.com>
2017-11-09 02:49:39 +0300

    endit queryset

--- town/views.py
+++ town/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
-from .models import Building, BuildingType
+from .models import BuildingType, Town
 from character.models import Character
 
 # Create your views here.
 
 
 def town(request):
-    town_id = (Character.objects.filter(user=request.user).values_list('town_id', flat=True))
-    building_id = (Building.objects.filter(town_id=town_id).values_list('building_type_id', flat=True))
-    building_list = (BuildingType.objects.filter(id=building_id).values_list('name', flat=True))[0]
-    return render(request, 'town/town.html', {'building_list': building_list})
+    town_name = (Town.objects.filter(character__user=request.user).values_list('name', flat=True))[0]
+    building_list = (BuildingType.objects.filter(building__town__character__user=request.user).values_list('name', flat=True))[0]
+    return render(request, 'town/town.html', {'town_name': town_name, 'building_list': building_list})
