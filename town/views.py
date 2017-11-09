382d20fe6f25390ac97ce43a9f200546ed50316f
Zagrebelsky Oleg <zagrebelsky.o@gmail.com>
2017-11-09 20:15:52 +0300

    endit queryset

--- town/views.py
+++ town/views.py
@@ -7,11 +7,9 @@ from django.db.models import Sum, F
 
 def town(request):
     town_name = (Town.objects.filter(character__user=request.user).values_list('name', flat=True))[0]
-    building_list = (BuildingType.objects.filter(building__town__character__user=request.user).values_list('name', flat=True))
-    building_ap = list(Building.objects.filter(town__character__user=request.user).values_list('ap', flat=True))
+    building_list = Building.objects.filter(building_type__building__town__character__user=request.user)
 #    get defence sum of all buildings. You get dict, then you get value and transform it into list
     defence = list((BuildingType.objects.filter(building__town__character__user=request.user).aggregate(Sum(F('defence')))).values())[0]
     return render(request, 'town/town.html', {'town_name': town_name,
                                               'building_list': building_list,
-                                              'building_ap': building_ap,
                                               'defence': defence, })
